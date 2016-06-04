3d25f11c0dfbd547efddaab80ba71781074117d3
Michael Baczynski <michaelbaczynski@gmail.com>
2016-06-04 17:25:06 +0200

    Add haxelib script

---
+++ tools/haxelib/run.bat
@@ -0,0 +1,37 @@
+@echo off
+if not exist ..\..\haxelib.json (
+	echo wrong working directory!
+	pause
+	exit
+)
+
+::create temporary output folders
+rmdir tmp /S /Q
+mkdir tmp
+mkdir tmp\git
+mkdir tmp\zip
+
+::glone git repository
+git clone -b master https://github.com/polygonal/printf.git tmp/git
+
+::make changes
+haxe -neko main.n -main Changes
+neko main.n tmp/git/README.md ./tmp/zip/CHANGES
+if errorlevel 1 (
+	echo ERROR
+	pause
+	exit
+)
+
+::copy git files
+xcopy tmp\git tmp\zip /E /exclude:exclude.txt
+
+::make haxelib
+haxe -neko main.n -main Zip -lib format
+neko main.n tmp/zip polygonal-printf.zip
+
+::clean
+rmdir tmp /S /Q
+del main.n
+
+sleep 3
